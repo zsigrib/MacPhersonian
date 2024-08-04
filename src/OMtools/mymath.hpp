4cfac8507d7ec50592fb60371e4bef6bf6ce01bd
zsigrib <zsigrib@gmail.com>
2024-08-04 12:26:53 +0200

    Add contained, contained_mask, and constants to NchooseK
    Former-commit-id: 09c61ed21bf77714ec92b2d03610d1bfb8ebe8e6

--- src/OMtools/mymath.hpp
+++ src/OMtools/mymath.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include<type_traits>
+#include <cstdint>
 
 // ====================
 // FORWARD DECLARATIONS
@@ -64,14 +65,26 @@ constexpr T division_rounded_up(T total, T divisor) {
     }
 }
 
-// NchooseK<N,k>::array lists all k-element subsets of {0..N-1}, 
-// and is calculated compile time.
+// NchooseK<N,k>::array lists all k-element subsets of {0..N-1}.
+// Auxulary constants are provided to make querying this list a
+// smoother experience. All constants are computed compile-time.
 template<typename T, T N, T k,
     std::enable_if_t<std::is_integral_v<T>, bool> = true>
 struct NchooseK {
+    // `(N choose R)=N!/(R!*(N-R)!)`, the number of distinct `R`-element 
+    // subsets of `0..N-1`.
+    constexpr static const auto NR_RTUPLES = binomial_coefficient(N,k);
+    // Number of (unsigned) 32-bit integers needed to have 1 bit for each 
+    // `R` element subset of `0..N-1`.
+    constexpr static const auto NR_INT32 = division_rounded_up(NR_RTUPLES, 32);
+    // Number of bits used in the last 32-bit unsigned integer used to 
+    // store the characteristic vector.
+    constexpr static const auto NR_REMAINING_BITS = NR_RTUPLES % 32;
+    // `array[i][t]` is the `t`th element of the `i`th `k`-element 
+    // subset of `0..N-1`. This is computed at compile time.
     constexpr static const auto array{[]() constexpr{
-        std::array<std::array<T, k>, binomial_coefficient(N,k)> a{}; 
-        for (int i = 0; i < k; i++) {
+        std::array<std::array<T, k>, NR_RTUPLES> a{}; 
+        for (T i = 0; i < k; i++) {
             a[0][i] = i;
         }
         T r = k -1;
@@ -89,5 +102,36 @@ struct NchooseK {
             s++;
         }
         return a;
+    }()};
+    // `contained[e][i]` is true if and only if the `i`th `k`-element
+    // subset of `0..N-1` contains the element `e`.
+    constexpr static const auto contained{[]() constexpr{
+        std::array<std::array<bool, NR_RTUPLES>, N> c{}; 
+        for (T e = 0; e < k; e++) {
+            for (T i = 0; i < NR_RTUPLES; i++) {
+                c[array[i][e]][i] = true;
+            }
+        }
+        return c;
+    }()};
+    // The `r`th lowest bit of `contained_mask32[e][idx]` is 1
+    // if and only if the `idx * 32 + r`th `k`-element subset
+    // of `0..N-1` contains the element `e`.
+    constexpr static const auto contained_mask32{[]() constexpr{
+        std::array<std::array<uint32_t, NR_INT32>, N> m{};
+        for (T e = 0; e < N; e++) {
+            for (auto idx = 0; idx < NR_INT32 - 1; idx++) {
+                for (auto r = 0; r < 32; r++) {
+                    m[e][idx] |= ((uint32_t)contained[e][idx * 32 + r]
+                        & (uint32_t)1) << r;
+                }
+            }
+            for (auto r = 0; r < NR_REMAINING_BITS; r++) {
+                m[e][NR_INT32 - 1] |= 
+                    ((uint32_t)contained[e][(NR_INT32 - 1) * 32 + r]
+                    & (uint32_t)1) << r;
+            }
+        }
+        return m;
     }()};
 };
