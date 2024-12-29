770b0a9fe206dee3ea02ac78e49acb656606a700
zsigrib <zsigrib@gmail.com>
2024-12-29 11:33:44 +0100

    Modify databasenames to reflect missing databases
    Former-commit-id: 974db6831b9d2f8fd5a36c02a6c33885b02d9a05

--- src/OMtools/databasenames.hpp
+++ src/OMtools/databasenames.hpp
@@ -9,16 +9,19 @@ namespace database_names {
 // which contains rank `R` chirotopes on `N` elements and with
 // `n_bases` many bases.
 template<int R, int N>
-std::string OM_set(int, int);
+std::string OM_set(int, int) {
+    static_assert(false, "There is no OM database for the specified parameters!");
+    return "";
+}
 
 template<>
-std::string OM_set<3,6>(int n_bases, int idx) {
+inline std::string OM_set<3,6>(int n_bases, int idx) {
     if (idx != 0) return "";
     return std::format("../../../resources/oriented_matroid_sets/r3n6/OMs_rank3_6elements_{}bases.txt", n_bases);
 }
 
 template<>
-std::string OM_set<3,7>(int n_bases, int idx) {
+inline std::string OM_set<3,7>(int n_bases, int idx) {
     if (n_bases < 27) {
         if (idx != 0) return "";
         return std::format("../../../resources/oriented_matroid_sets/r3n7/OMs_rank3_7elements_{}bases.txt", n_bases);
